2ec54857376e319e6b09351ed844ea75ab9656d6
renatabg2205 <renatabg@gmail.com>
2021-04-03 15:59:59 -0500

    Adding Vine_Review_Analysis.sql and Updating Vine_Review_Analysis.ipynb

---
+++ Vine_Review_Analysis.sql
@@ -0,0 +1,13 @@
+-- vine table
+CREATE TABLE vine_table (
+  review_id TEXT PRIMARY KEY,
+  star_rating INTEGER,
+  helpful_votes INTEGER,
+  total_votes INTEGER,
+  vine TEXT,
+  verified_purchase TEXT
+);
+
+-- Read table
+SELECT * FROM vine_table;
+SELECT * FROM vine_table limit 5;
